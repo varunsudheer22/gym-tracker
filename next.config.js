4fe59d1a30e3d2383c7e157708197e2dac857ab3
Varun Sudheer <your.email@example.com>
2025-02-11 15:37:20 +0930

    Update production configuration

--- next.config.js
+++ next.config.js
@@ -35,10 +35,20 @@ const nextConfig = {
             key: 'Referrer-Policy',
             value: 'origin-when-cross-origin',
           },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+          }
         ],
       },
     ];
   },
+  // Output static files when possible
+  output: 'standalone',
 }
 
 module.exports = nextConfig 
